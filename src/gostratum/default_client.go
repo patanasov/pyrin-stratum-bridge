e750f8848464684ecce7fe7c53cb6e2637f6e50b
Pavel Atanasov <pavel.v.atanasov@gmail.com>
2023-12-13 18:25:40 +0200

    change wallet check to match Pyrin network

--- src/gostratum/default_client.go
+++ src/gostratum/default_client.go
@@ -111,20 +111,20 @@ func SendExtranonce(ctx *StratumContext) {
 	}
 }
 
-var walletRegex = regexp.MustCompile("kaspa:[a-z0-9]+")
+var walletRegex = regexp.MustCompile("pyrin:[a-z0-9]+")
 
 func CleanWallet(in string) (string, error) {
 	_, err := util.DecodeAddress(in, util.Bech32PrefixKaspa)
 	if err == nil {
 		return in, nil // good to go
 	}
-	if !strings.HasPrefix(in, "kaspa:") {
-		return CleanWallet("kaspa:" + in)
+	if !strings.HasPrefix(in, "pyrin:") {
+		return CleanWallet("pyrin:" + in)
 	}
 
-	// has kaspa: prefix but other weirdness somewhere
+	// has pyrin: prefix but other weirdness somewhere
 	if walletRegex.MatchString(in) {
 		return in[0:67], nil
 	}
-	return "", errors.New("unable to coerce wallet to valid kaspa address")
+	return "", errors.New("unable to coerce wallet to valid pyrin address")
 }
